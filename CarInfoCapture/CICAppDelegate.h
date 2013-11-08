39408a9f231e8aeccab059afcdb90d9b61c302f2
Liu Feng <gumeit@gmail.com>
2013-11-08 10:28:39 +0800

    Initial Commit

---
+++ CarInfoCapture/CICAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CICAppDelegate.h
+//  CarInfoCapture
+//
+//  Created by Liu Feng on 13-11-8.
+//  Copyright (c) 2013年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CICAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarInfoCapture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarInfoCapture
+//
+//  Created by Liu Feng on 13-11-8.
+//  Copyright (c) 2013年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CICAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CICAppDelegate class]));
+    }
+}
