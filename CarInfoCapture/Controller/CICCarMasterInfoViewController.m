d51f8a99c6c0ea2fee83baa39c3ce632c01b6c89
Liu Feng <gumeit@gmail.com>
2013-11-25 18:50:48 +0800

    update

--- CarInfoCapture/Controller/CICCarMasterInfoViewController.m
+++ CarInfoCapture/Controller/CICCarMasterInfoViewController.m
@@ -26,7 +26,7 @@
     UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
     numberToolbar.barStyle = UIBarStyleDefault;
     numberToolbar.items = [NSArray arrayWithObjects:
-                           [[UIBarButtonItem alloc]initWithTitle:@"取消" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
+                           [[UIBarButtonItem alloc]initWithTitle:@"清空" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
                            [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
                            [[UIBarButtonItem alloc]initWithTitle:@"确定" style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
                            nil];
@@ -40,19 +40,41 @@
 {
     self.carInfoEntity.masterName = self.masterNameTextField.text;
     
+    [self.delegate carInfoDidChange:self.carInfoEntity];
+    
+    return YES;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField
+{
+    [self updateCarInfoEntity];
     
     return YES;
 }
 
 #pragma mark - Private
 
--(void)cancelNumberPad{
-    [self.masterTelTextField resignFirstResponder];
+- (void)updateCarInfoEntity
+{
+    self.carInfoEntity.masterName = self.masterNameTextField.text;
+    self.carInfoEntity.masterTel = self.masterTelTextField.text;
+    
+    [self.delegate carInfoDidChange:self.carInfoEntity];
 }
 
--(void)doneWithNumberPad{
+- (void)cancelNumberPad{
+    [self.masterTelTextField resignFirstResponder];
+    self.masterTelTextField.text = @"";
+    self.carInfoEntity.masterTel = self.masterTelTextField.text;
+    
+    [self.delegate carInfoDidChange:self.carInfoEntity];
+}
+
+- (void)doneWithNumberPad{
     self.carInfoEntity.masterTel = self.masterTelTextField.text;
     [self.masterTelTextField resignFirstResponder];
+    
+    [self.delegate carInfoDidChange:self.carInfoEntity];
 }
 
 - (void)updateViewWithCarInfo
